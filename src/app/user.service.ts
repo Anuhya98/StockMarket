83e715708862ac59088fd04a4f37ba03b8ce9f04
Anuhya98 <anuhya2641@gmail.com>
2020-03-02 18:26:08 +0530

    day51

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -9,7 +9,7 @@ import { UsersComponent } from './users/users.component';
   providedIn: 'root'
 })
 export class UserService {
-  httpUrl = 'http://localhost:8080/users/';
+  httpUrl = 'http://localhost:8001/users/';
 
   constructor(private httpClient: HttpClient,@Inject(HttpClient)  private ht) { }
   getALLUsers(): Observable<User[]> {
@@ -19,22 +19,22 @@ export class UserService {
     return this.ht.post(this.httpUrl,user);
   }
   reg(){
-    return this.ht.get(`http://localhost:8080/usersreg`);
+    return this.ht.get(`http://localhost:8001/usersreg`);
   }
   deleteUser(id : number):Observable<User>{
     //return this.httpClient.delete<User>(this.httpUrl + id);
-    return this.ht.delete(`http://localhost:8080/users/${id}`);
+    return this.ht.delete(`http://localhost:8001/users/${id}`);
   }
   updateUserInfo(user:User):Observable<User>{
     //return this.httpClient.put<User>(this.httpUrl+user.id,user);
-    return this.ht.put(`http://localhost:8080/users`,user);
+    return this.ht.put(`http://localhost:8001/updateusers`,user);
   }
   getUserById(id:number):Observable<User>{
    // return this.httpClient.get<User>(this.httpUrl+id);
-   return this.ht.get(`http://localhost:8080/users/${id}`)
+   return this.ht.get(`http://localhost:8001/users/${id}`)
   }
   serActivation(obj){
-    return this.ht.put("http://localhost:8080/users/activate",obj)
+    return this.ht.put("http://localhost:8001/users/activate",obj)
   }
 
 }
